a9c75de76f458e4d37c049827d637823d96f0dc0
Jaime Santa <Santa0779@gmail.com>
2022-02-21 23:17:48 -0600

    corrigiendo errores en nombres

--- animales-Jaime/app.js
+++ animales-Jaime/app.js
@@ -1,52 +1,51 @@
-const animalesSuperior = document.querySelectorAll('#AnimalesColumnaSuperior')
+const animalesColumnaSuperior = document.querySelectorAll('#AnimalesColumnaSuperior')
 
 
-animalesSuperior[0].addEventListener('click',function(){
+animalesColumnaSuperior[0].addEventListener('click',function(){
    new Audio('assets/sonidos/avispa.mp3').play();
 });
-animalesSuperior[1].addEventListener('click',function(){
+animalesColumnaSuperior[1].addEventListener('click',function(){
     new Audio('assets/sonidos/delfin.mp3').play();
  });
 
- animalesSuperior[2].addEventListener('click',function(){
+ animalesColumnaSuperior[2].addEventListener('click',function(){
     new Audio('assets/sonidos/foca.mp3').play();
  });
 
- animalesSuperior[3].addEventListener('click',function(){
+ animalesColumnaSuperior[3].addEventListener('click',function(){
     new Audio('assets/sonidos/grillo.mp3').play();
  });
 
- animalesSuperior[4].addEventListener('click',function(){
+ animalesColumnaSuperior[4].addEventListener('click',function(){
     new Audio('assets/sonidos/koala.mp3').play();
  });
 
- const animalesInferior = document.querySelectorAll('#AnimalesColumnaInferior')
+ const animalesColumnaInferior = document.querySelectorAll('#AnimalesColumnaInferior')
 
 
- animalesInferior[0].addEventListener('click',function(){
+ animalesColumnaInferior[0].addEventListener('click',function(){
     new Audio('assets/sonidos/lince.mp3').play();
  });
 
- animalesInferior[1].addEventListener('click',function(){
+ animalesColumnaInferior[1].addEventListener('click',function(){
     new Audio('assets/sonidos/lobo.mp3').play();
  });
- animalesInferior[2].addEventListener('click',function(){
+ animalesColumnaInferior[2].addEventListener('click',function(){
     new Audio('assets/sonidos/penguin.mp3').play();
  });
- animalesInferior[3].addEventListener('click',function(){
+ animalesColumnaInferior[3].addEventListener('click',function(){
     new Audio('assets/sonidos/rana.mp3').play();
  });
- animalesInferior[4].addEventListener('click',function(){
+ animalesColumnaInferior[4].addEventListener('click',function(){
     new Audio('assets/sonidos/zorro.mp3').play();
  });
 
 
  document.addEventListener('keypress',function(e){
-    reproducirAnimal(e.key);
+    reproducirSonidoAnimal(e.key);
 });
 
-    const reproducirAnimal = (tecla) =>{
-
+    const reproducirSonidoAnimal = (tecla) =>{
         if(tecla == 'a'){
             new Audio('assets/sonidos/avispa.mp3').play();
         }else if(tecla == 's'){
@@ -59,7 +58,6 @@ animalesSuperior[1].addEventListener('click',function(){
             new Audio('assets/sonidos/koala.mp3').play();
         }else if(tecla == 'z'){
             new Audio('assets/sonidos/lince.mp3').play();
-            stopAudio();
         }else if(tecla == 'x'){
             new Audio('assets/sonidos/lobo.mp3').play();
         }else if(tecla == 'c'){
@@ -69,5 +67,4 @@ animalesSuperior[1].addEventListener('click',function(){
         }else if(tecla == 'b'){
             new Audio('assets/sonidos/zorro.mp3').play();
         }
-        
      };
